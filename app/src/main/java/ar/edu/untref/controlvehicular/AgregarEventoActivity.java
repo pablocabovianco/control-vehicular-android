665f15603237f94d54a2db44b7ee4035b5bcb9f6
Ivan Mengual <ivanmengual97@gmail.com>
2023-05-26 11:53:13 -0300

    Se agrega manejo de eventos por fecha

--- app/src/main/java/ar/edu/untref/controlvehicular/AgregarEventoActivity.java
+++ app/src/main/java/ar/edu/untref/controlvehicular/AgregarEventoActivity.java
@@ -1,18 +1,23 @@
 package ar.edu.untref.controlvehicular;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -20,8 +25,8 @@ public class AgregarEventoActivity extends AppCompatActivity {
     EventosViewModel viewModel;
 
     private ArrayAdapter<String> mAdapter;
-    //Seteo valores de LV
     Spinner tipoEvento;
+    String fechaDeEvento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +41,15 @@ public class AgregarEventoActivity extends AppCompatActivity {
 
         tipoEvento.setAdapter(adapter);
 
+        EditText fechaDeEvento = (EditText) findViewById(R.id.fechaEvento);
+        fechaDeEvento.setOnClickListener(view -> {
+            DatePickerFragment newFragment = new DatePickerFragment();
+            newFragment.show(getSupportFragmentManager(), "datePicker");
+        });
+
         Button agregarBtn = findViewById(R.id.btnGuardarEvento);
-        agregarBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                agregarEvento();
-            }
-        });
-        findViewById(R.id.btnCancelar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {onCancelar();}
-        });
+        agregarBtn.setOnClickListener(view -> agregarEvento());
+        findViewById(R.id.btnCancelar).setOnClickListener(view -> onCancelar());
 
 
     }
@@ -57,12 +60,18 @@ public class AgregarEventoActivity extends AppCompatActivity {
 
         EditText campoKilometros = (EditText) findViewById(R.id.textKilometros);
         int kilometros = Integer.parseInt(campoKilometros.getText().toString());
+        Boolean esPorKm;
         //Asigno segun el tipo para cargar en la tabla
-        Boolean esPorKm = tipoEvento.getSelectedItem().toString() == "Por Kilometros" ? Boolean.TRUE : Boolean.FALSE;
-
+        if (tipoEvento.getSelectedItem().toString() == "Por Kilometros"){
+            esPorKm = Boolean.TRUE;
+            fechaDeEvento = "0";
+        } else {
+            esPorKm = Boolean.FALSE;
+            kilometros = 0;
+        }
         //Creo el evento
         //TODO debe agregar kilometros totales + los ingresados
-        Eventos nuevoEvento = new Eventos(titulo,esPorKm,0, kilometros);
+        Eventos nuevoEvento = new Eventos(titulo,esPorKm,Integer.parseInt(fechaDeEvento), kilometros);
 
         //Lo agrego a la base y actualizo la lista
         this.viewModel.insertEvento(nuevoEvento);
@@ -80,5 +89,27 @@ public class AgregarEventoActivity extends AppCompatActivity {
         //Vuelvo a la pantalla anterior
         Intent intent = new Intent(this, MostrarEventosActivity.class);
         startActivity(intent);
+    }
+    //Manejo de fecha
+    public class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            fechaDeEvento = "" + year + month + day;
+        }
+
     }
 }
