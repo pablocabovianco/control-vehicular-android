866686ff012cf6cec66434808332121ae48b46a0
Mariano <rathalosrathalos@hotmail.com>
2023-06-25 17:37:37 -0300

    logea para enviar mails de vencimiento de eventos

--- app/src/main/java/ar/edu/untref/controlvehicular/EnviadorMailsActivity.java
+++ app/src/main/java/ar/edu/untref/controlvehicular/EnviadorMailsActivity.java
@@ -4,8 +4,10 @@ import android.os.AsyncTask;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -14,15 +16,23 @@ import javax.mail.internet.MimeMessage;
 public class EnviadorMailsActivity extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... voids) {
+
+        final String EMAIL_USERNAME = "tu_correo@gmail.com";
+        final String EMAIL_PASSWORD = "tu_contraseña";
+
         Properties properties = new Properties();
         properties.put("mail.smtp.host", "smtp.mailinator.com");
         properties.put("mail.smtp.port", "25");
-
-        Session session = Session.getInstance(properties);
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(EMAIL_USERNAME, EMAIL_PASSWORD);
+            }
+        });
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("tuCorreo@mailinator.com"));
+            message.setFrom(new InternetAddress(EMAIL_USERNAME));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("otroCorreo@mailinator.com"));
             message.setSubject("Prueba de correo desde Mailinator");
             message.setText("Hola,\n\nEste es un correo de prueba enviado desde Mailinator.");
